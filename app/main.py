0c21819ca6061b648d243e8c491804958ce25665
lcw0910 <chlwn.lee@gmail.com>
2024-02-04 17:20:47 +0900

    Update FastAPI project structure and settings
    This update makes several key changes. It includes the introduction of specific package versions in the requirements.txt file, and the creation of a new .gitignore file. Additionally, SSL certificates are integrated in the Nginix Docker setting and Nginix configuration files are revised accordingly. The FastAPI application is also extended to support Server-Sent Events and OpenAPI documentation customized. Lastly, code comments and documentation including README files are updated for clarity and to reflect the changes made.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
+import time
+from datetime import datetime
 from typing import Optional
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
+from fastapi.openapi.utils import get_openapi
+from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 
 app = FastAPI(title="FastAPI Example")
@@ -21,6 +25,34 @@ def read_root():
     }
 
 
+@app.get("/stream")
+async def stream(token: Optional[str] = Header(None)):
+
+    timeout = 5
+    connected_time = datetime.now()
+
+    def check_for_event():
+        if (datetime.now() - connected_time).seconds > timeout:
+            return "SUCCESS"
+        return False
+
+    def event_stream():
+        count = 0
+        while True:
+            if check_for_event() == "SUCCESS":
+                yield f"data: {count}\n\n"
+
+            time.sleep(1)
+            count += 1
+
+            if count > 10:
+                return
+
+
+    generator = event_stream()
+    return StreamingResponse(generator, media_type="text/event-stream")
+
+
 @app.post("/items/")
 async def create_item(item: Item):
     item_dict = item.dict()
@@ -29,6 +61,25 @@ async def create_item(item: Item):
         item_dict.update({"price_with_tax": price_with_tax})
     return item_dict
 
+
+openapi_schema = get_openapi(
+    title="Custom title",
+    version="0.0.1",
+    description="This is a very custom OpenAPI schema",
+    routes=app.routes,
+    servers=[
+        {
+            "url": "http://yicheolwon.org",
+            "description": "Local",
+        },
+        {
+            "url": "https://api.example.com",
+            "description": "Production server",
+        },
+    ]
+)
+
+app.openapi_schema = openapi_schema
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
---
+++ docker/nginx/README.md
@@ -0,0 +1,9 @@
+# Nginx
+
+## SSL 인증서 적용하기
+```shell
+# efaeoifjawe;ofij
+$ cd nginx/ssl
+$ brew install mkcert
+$ mkcert -install -key-file key.pem -cert-file cert.pem "*.first-your-domain.com" "*.second-your-domain.com"
+```
